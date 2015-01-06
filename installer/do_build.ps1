378db691c1d6a6bdac80e84ea38a753b91c776de
David Staelens <staelensd@ainfosec.com>
2015-01-06 15:08:14 -0500

    Fixed issues with certificate signing and the installer.
    Signed-off-by: David Staelens <staelensd@ainfosec.com>

--- installer/do_build.ps1
+++ installer/do_build.ps1
@@ -33,14 +33,17 @@ $BuildTag = $argtable["BuildTag"]
 $VerString = $argtable["VerString"]
 $CertName = $argtable["CertName"]
 $Company = $argtable["CompanyName"]
+$signtool = $argtable["SignTool"]
+
+$env:SIGNTOOLPATH = ($signtool+"\signtool.exe")
 
 cd msi-installer\installer
 Invoke-CommandChecked "32 bit candle" ($env:WIX + "bin\candle.exe") installer.wxs -dPlatform="x86" ("-dCompany=" + $Company) ("-dPRODUCT_VERSION=" + $VerString) ("-dTAG=" + $BuildTag) -out XenClientTools.wixobj
 Invoke-CommandChecked "32 bit light" ($env:WIX + "bin\light.exe") -sw1076 -ext WixUIExtension XenClientTools.wixobj -out XenClientTools.msi -cc cache -reusecab
 Invoke-CommandChecked "64 bit candle" ($env:WIX + "bin\candle.exe") installer.wxs -dPlatform="x64" ("-dCompany=" + $Company) ("-dPRODUCT_VERSION=" + $VerString) ("-dTAG=" + $BuildTag) -out XenClientTools64.wixobj
 Invoke-CommandChecked "64 bit light" ($env:WIX + "bin\light.exe") -sw1076 -ext WixUIExtension XenClientTools64.wixobj -out XenClientTools64.msi -cc cache -reusecab
-Invoke-CommandChecked "sign 32 bit MSI" signtool.exe sign /a /s my /n $CertName /t http://timestamp.verisign.com/scripts/timestamp.dll /d "$Company XenClient Tools Installer" XenClientTools.msi
-Invoke-CommandChecked "sign 64 bit MSI" signtool.exe sign /a /s my /n $CertName /t http://timestamp.verisign.com/scripts/timestamp.dll /d "$Company XenClient Tools Installer" XenClientTools64.msi
+Invoke-CommandChecked "sign 32 bit MSI" ($signtool+"\signtool.exe") sign /a /s my /n ('"'+$CertName+'"') /t http://timestamp.verisign.com/scripts/timestamp.dll /d "$Company XenClient Tools Installer" XenClientTools.msi
+Invoke-CommandChecked "sign 64 bit MSI" ($signtool+"\signtool.exe") sign /a /s my /n ('"'+$CertName+'"') /t http://timestamp.verisign.com/scripts/timestamp.dll /d "$Company XenClient Tools Installer" XenClientTools64.msi
 
 # Copy Signer's Certificate to ISO
 $CertOutPath = (Convert-Path ..\iso\windows\SupportFiles\) + "ToolsSigner.cer"
